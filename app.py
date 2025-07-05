addea73c73a0bbe7ce34a430a4507b5e33603bb5
Pedro Saldaña <2005pedrosaldana@gmail.com>
2025-07-05 03:43:37 -0400

    Primera versión de MecaSolver

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import spacy
+
+app = Flask(__name__)
+nlp = spacy.load('es_core_news_sm')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    description = data.get('description', '')
+    doc = nlp(description)
+    return jsonify({"result": "Análisis completado", "description": description})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
